b1033a6569a95a1ae9ab325b5e73444445e1c315
Shreyash S Sarnayak <shreyash14s@gmail.com>
2017-11-12 00:21:42 +0530

    Formatted it.

--- odbaas/src/com/odbaas/Server.java
+++ odbaas/src/com/odbaas/Server.java
@@ -22,16 +22,11 @@ public class Server {
 	@Context
 	private ServletContext sctx;
 	
-        
-        @GET
+	@GET
 	@Path("mytest/{param}")
-	public Response getMsg(@PathParam("param") String msg) 
-        {
-
+	public Response getMsg(@PathParam("param") String msg) {
             String output = "Jersey say : " + msg;
-
             return Response.status(200).entity(output).build();
-
 	}
         
         
@@ -39,96 +34,94 @@ public class Server {
 	@POST
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces("application/json")
-	public Response login(@FormParam("user_name") String userName, @FormParam("password") String password)
-	{
-		return null;
-		//something
-			
+	public Response login(@FormParam("user_name") String userName, 
+			@FormParam("password") String password) {
+		return Response.status(200).entity("reiuhg").build();
 	}
 	
 	@Path("/table/create/{table_name}")
 	@POST
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces("application/json")
-	public Response createTable(@PathParam("table_name") String tableName, @FormParam("token") String token, @FormParam("schema") String schema, @FormParam("primary_key") String pKey)
-	{
+	public Response createTable(@PathParam("table_name") String tableName, 
+			@FormParam("token") String token, @FormParam("schema") String schema, 
+			@FormParam("primary_key") String pKey) {
 		return null;
 		//something
-			
 	}
 	
 	@Path("/table/insert/{table_name}")
 	@POST
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces("application/json")
-	public Response insertTable(@PathParam("table_name") String tableName, @FormParam("token") String token, @FormParam("schema") String schema,@FormParam("data") String data)
-	{
+	public Response insertTable(@PathParam("table_name") String tableName, 
+			@FormParam("token") String token, @FormParam("schema") String schema, 
+			@FormParam("data") String data) {
 		return null;
 		//something
-			
 	}
 	
 	/*
 	@Path("/mytry")
 	@GET
     @Produces(MediaType.TEXT_PLAIN)
-	public String mytry()
-	{
+	public String mytry() {
 		return "Hi there";
 	}
 	
 	@Path("/select")
 	@GET
 	@Produces("application/json")
-	public Response selectTable(@QueryParam("table_name") String tableName, @QueryParam("token") String token, @QueryParam("columns") String columns,@QueryParam("where") String where,@QueryParam("sort") String sort, @QueryParam("limit") String limit )
-	{
+	public Response selectTable(@QueryParam("table_name") String tableName, 
+			@QueryParam("token") String token, 
+			@QueryParam("columns") String columns,
+			@QueryParam("where") String where, @QueryParam("sort") String sort, 
+			@QueryParam("limit") String limit) {
 		return null;
 		//something
-			
 	}
 	*/
+	
 	@Path("/select")
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String selectTable()
-	{
+	public String selectTable() {
 		return "Hi there";
 		//something
-			
 	}
+	
 	@Path("/update/{table_name}")
 	@PUT
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED )
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces("application/json")
-	public Response updateTable(@PathParam("table_name") String tableName, @FormParam("token") String token, @QueryParam("columns") String columns,@QueryParam("where") String where,@QueryParam("sort") String sort, @QueryParam("limit") String limit )
-	{
+	public Response updateTable(@PathParam("table_name") String tableName, 
+			@FormParam("token") String token, 
+			@QueryParam("columns") String columns, 
+			@QueryParam("where") String where, 
+			@QueryParam("sort") String sort, 
+			@QueryParam("limit") String limit) {
 		return null;
 		//something
-			
 	}
 	
 	@Path("/delete/{table_name}")
 	@POST
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces("application/json")
-	public Response deleteTable(@PathParam("table_name") String tableName, @FormParam("token") String token,@QueryParam("where") String where )
-	{
+	public Response deleteTable(@PathParam("table_name") String tableName, 
+			@FormParam("token") String token, 
+			@QueryParam("where") String where) {
 		return null;
 		//something
-			
 	}
 	
 	@Path("/drop/{table_name}")
 	@POST
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces("application/json")
-	public Response dropTable(@PathParam("table_name") String tableName, @FormParam("token") String token )
-	{
+	public Response dropTable(@PathParam("table_name") String tableName, 
+			@FormParam("token") String token) {
 		return null;
 		//something
-			
 	}
-	
 }
-
-
